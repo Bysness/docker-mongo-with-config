ecb30bd726f9b5a8eb432c554231eff2dfd83ba7
Santosh Thombare <santosh@bysness.com>
2017-12-24 13:06:44 +0530

    Changed paths relative to the repository

--- docker_mongo_replicaset_commands.sh
+++ docker_mongo_replicaset_commands.sh
@@ -1,28 +1,32 @@
 echo "Creating volumes."
 
 #create volumes
-sudo docker volume create --name vol_mongo_repl_alpha0 && sudo docker volume create --name vol_mongo_repl_alpha1 && sudo docker volume create --name vol_mongo_repl_alpha2 && sudo docker volume create --name vol_mongo_repl_beta0 && sudo docker volume create --name vol_mongo_repl_beta1 && sudo docker volume create --name vol_mongo_repl_beta2
+sudo docker volume create --name vol_mongo_repl_alpha0 && \
+sudo docker volume create --name vol_mongo_repl_alpha1 && \
+sudo docker volume create --name vol_mongo_repl_alpha2 && \
+sudo docker volume create --name vol_mongo_repl_beta0 && \
+sudo docker volume create --name vol_mongo_repl_beta1 && \
+sudo docker volume create --name vol_mongo_repl_beta2
 
 echo "Volumes created."
 echo "Building images"
 #build mongo images with a config file
 
 #create alpha0 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_alpha/n0/config.yaml ./config/an0.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an0.yaml --tag mongo_repl_alpha0 $HOME/docker/mongo-with-config
-
+mkdir -p ./config/ && cp ./repl_alpha/n0/config.yaml ./config/an0.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an0.yaml --tag mongo_repl_alpha0 $HOME/docker/mongo-with-config
 #create alpha1 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_alpha/n1/config.yaml ./config/an1.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an1.yaml --tag mongo_repl_alpha1 $HOME/docker/mongo-with-config
+mkdir -p ./config/ && cp ./repl_alpha/n1/config.yaml ./config/an1.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an1.yaml --tag mongo_repl_alpha1 $HOME/docker/mongo-with-config
 #create alpha2 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_alpha/n2/config.yaml ./config/an2.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an2.yaml --tag mongo_repl_alpha2 $HOME/docker/mongo-with-config
+mkdir -p ./config/ && cp ./repl_alpha/n2/config.yaml ./config/an2.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/an2.yaml --tag mongo_repl_alpha2 $HOME/docker/mongo-with-config
 
 echo "Built images 3/6"
 
 #create beta0 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_beta/n0/config.yaml ./config/bn0.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn0.yaml --tag mongo_repl_beta0 $HOME/docker/mongo-with-config
+mkdir -p ./config/ && cp ./repl_beta/n0/config.yaml ./config/bn0.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn0.yaml --tag mongo_repl_beta0 $HOME/docker/mongo-with-config
 #create beta1 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_beta/n1/config.yaml ./config/bn1.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn1.yaml --tag mongo_repl_beta1 $HOME/docker/mongo-with-config
+mkdir -p ./config/ && cp ./repl_beta/n1/config.yaml ./config/bn1.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn1.yaml --tag mongo_repl_beta1 $HOME/docker/mongo-with-config
 #create beta2 image
-mkdir -p ./config/ && cp $HOME/docker/mongo/repl_beta/n2/config.yaml ./config/bn2.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn2.yaml --tag mongo_repl_beta2 $HOME/docker/mongo-with-config
+mkdir -p ./config/ && cp ./repl_beta/n2/config.yaml ./config/bn2.yaml && sudo docker build --build-arg SOURCE_CONFIG_FILE=./config/bn2.yaml --tag mongo_repl_beta2 $HOME/docker/mongo-with-config
 
 echo "Built images 6/6"
 
